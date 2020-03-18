cbc2391523eeabe6f32205292eeecf406c1bba3b
Betsad <amedgr@gmail.com>
2020-03-18 09:49:43 -0600

    Este es el primer commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
-// const HtmlWebpackPlugin = require('html-webpack-plugin') /* Se exporta el plugin de HTML que se acaba de instalar */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
 module.exports = {
-  /* El output de la compilación va a estar en el archivo: */
+  mode: 'development',
   output: {
     filename: 'app.bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    }) /* De esta forma utilizamos la instancia de este plugin  */
+    })
   ],
   module: {
-    /* Reglas que queremos que tengan nuestros módulos */
+
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$/, // añadir regla para todos los archivos que sean de javascript
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
